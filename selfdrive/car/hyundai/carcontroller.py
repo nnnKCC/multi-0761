642294d9038bd184e5a338f444b81b84b33db042
KCC <nonghost@nonghost.com>
2020-10-21 16:41:10 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -344,20 +344,39 @@ class CarController():
     if pcm_cancel_cmd and self.CP.longcontrolEnabled:
       can_sends.append(create_clu11(self.packer, frame, CS.clu11, Buttons.CANCEL))
 
-    elif CS.out.cruiseState.standstill:
-      # run only first time when the car stopped
-      if self.last_lead_distance == 0 or not self.param_OpkrAutoResume:
-        # get the lead distance from the Radar
+# fix auto resume - by neokii
+    if CS.out.cruiseState.standstill:
+      if self.last_lead_distance == 0:
         self.last_lead_distance = CS.lead_distance
         self.resume_cnt = 0
-      # when lead car starts moving, create 6 RES msgs
-      elif CS.lead_distance != self.last_lead_distance and (frame - self.last_resume_frame) > 5:
+        self.resume_wait_timer = 0
+
+      elif self.resume_wait_timer > 0:
+        self.resume_wait_timer -= 1
+
+      elif CS.lead_distance != self.last_lead_distance:
         can_sends.append(create_clu11(self.packer, frame, CS.scc_bus, CS.clu11, Buttons.RES_ACCEL, clu11_speed))
         self.resume_cnt += 1
-        # interval after 6 msgs
+
         if self.resume_cnt > 5:
-          self.last_resume_frame = frame
           self.resume_cnt = 0
+          self.resume_wait_timer = int(0.2 / DT_CTRL)
+
+#    elif CS.out.cruiseState.standstill:
+#      # run only first time when the car stopped
+#      if self.last_lead_distance == 0 or not self.param_OpkrAutoResume:
+#        # get the lead distance from the Radar
+#        self.last_lead_distance = CS.lead_distance
+#        self.resume_cnt = 0
+#      # when lead car starts moving, create 6 RES msgs
+#      elif CS.lead_distance != self.last_lead_distance and (frame - self.last_resume_frame) > 5:
+#        can_sends.append(create_clu11(self.packer, frame, CS.scc_bus, CS.clu11, Buttons.RES_ACCEL, clu11_speed))
+#        self.resume_cnt += 1
+#        # interval after 6 msgs
+#        if self.resume_cnt > 5:
+#          self.last_resume_frame = frame
+#          self.resume_cnt = 0
+
     # reset lead distnce after the car starts moving
     elif self.last_lead_distance != 0:
       self.last_lead_distance = 0
