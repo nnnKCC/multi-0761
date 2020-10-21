75980404f3bc20ef483ce2f5a05ab05228cfdbe4
KCC <nonghost@nonghost.com>
2020-10-21 18:07:20 +0900

    Update hyundaican.py

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -73,7 +73,7 @@ def create_clu11(packer, frame, bus, clu11, button, speed = None):
     values["CF_Clu_Vanz"] = speed
 
   values["CF_Clu_CruiseSwState"] = button
-  values["CF_Clu_AliveCnt1"] = frame % 0x10
+  values["CF_Clu_AliveCnt1"] = frame // 2 % 0x10
   return packer.make_can_msg("CLU11", bus, values)
 
 
