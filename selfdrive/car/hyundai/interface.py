d130c0e8201d7f41956c1331a905d785804e84cc
KCC <nonghost@nonghost.com>
2020-10-22 12:34:34 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -198,14 +198,14 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
     elif candidate == CAR.NIRO_EV:
       ret.lateralTuning.init('lqr')
-      ret.lateralTuning.lqr.scale = 1550.0
+      ret.lateralTuning.lqr.scale = 1600.0
       ret.lateralTuning.lqr.ki = 0.002
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
       ret.lateralTuning.lqr.c = [1., 0.]
-      ret.lateralTuning.lqr.k = [-110., 451.]
+      ret.lateralTuning.lqr.k = [-95., 451.]
       ret.lateralTuning.lqr.l = [0.33, 0.318]
-      ret.lateralTuning.lqr.dcGain = 0.002870
+      ret.lateralTuning.lqr.dcGain = 0.00275
       #ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1755. + STD_CARGO_KG
       ret.wheelbase = 2.7
